6d8ca78a9b0e1e91c490d9ee4186a4efc6db7095
H3set-py <66518492+H3set-py@users.noreply.github.com>
2020-06-29 17:36:37 +0300

    Add SQRT

--- CalculatorAutoTest.py
+++ CalculatorAutoTest.py
@@ -17,13 +17,14 @@ app = Application().start(path + "Calculator_1.exe")
 report = open(path + "Result.txt", 'w')
 
 #Генерация тестового массива
-list1 = np.arange(1,100)
-list2 = np.logspace(2,9,dtype='int')
+list1 = np.arange(1, 10)
+list2 = np.logspace(2, 9, dtype = 'int')
 
 #Объеденение массивов
 joinedList = np.concatenate([list1, list2]) 
 
 #Проверка "LogBase"
+print("-----LOG10-----")
 for n in joinedList:
     app.Calculator.Edit.set_text(n)
     app.Calculator.button.click()
@@ -37,7 +38,30 @@ for n in joinedList:
     resultMath = round(resultMath, 14)
     
     #Вывод результата
-    textResult = 'Ввод - ' + str(n) + ', ln10, ' + str(resultApp) + ' | ' + str(resultMath)
+    textResult = 'Ввод - ' + str(n) + ', log10, ' + str(resultApp) + ' | ' + str(resultMath)
+
+    if resultApp == resultMath:
+        print(textResult + colored(' OK', 'green'))
+    else:
+        report.write(textResult + ' ERROR' "\n")
+        print(textResult + colored(' ERROR', 'red'))
+
+#Проверка "Square"
+print("-----SQRT-----")
+for n in joinedList:
+    app.Calculator.Edit.set_text(n)
+    app.Calculator.button2.click()
+    
+    #Получение результата из программы
+    texts = app.Calculator.Static2.texts()
+    resultApp = float(texts[0].replace(',', '.'))
+    
+    #Расчет ожидаемого результата
+    resultMath = math.sqrt(n)
+    resultMath = round(resultMath, 14)
+    
+    #Вывод результата
+    textResult = 'Ввод - ' + str(n) + ', sqrt, ' + str(resultApp) + ' | ' + str(resultMath)
 
     if resultApp == resultMath:
         print(textResult + colored(' OK', 'green'))
